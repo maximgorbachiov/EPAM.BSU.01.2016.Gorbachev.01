d86962f046fb89c0ac25b2ee070f2efa72273590
maximgorbachiov <maksimka.gorbachev.95.gmail.com>
2016-03-20 04:01:49 +0300

    Новая реализация сортировки

---
+++ Sorter.NUnit.Tests/SorterTests.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Sorter.NUnit.Tests
+{
+    [TestFixture]
+    public class SorterTests
+    {
+        class AscendingComparator : IComparator
+        {
+            public bool Compare(int a, int b)
+            {
+                return a > b;
+            }
+        }
+
+        class SumSortedSign: ISortedSign
+        {
+            public int GetSortedSign(int[] array)
+            {
+                return array.Sum();
+            }
+        }
+
+        class DiscendingComparator : IComparator
+        {
+            public bool Compare(int a, int b)
+            {
+                return a < b;
+            }
+        }
+
+        class MaxElementSortedSign : ISortedSign
+        {
+            public int GetSortedSign(int[] array)
+            {
+                return array.Max();
+            }
+        }
+
+        [TestCase]
+        public void TestSortOfJaggedArrayByAscendingOfSumElements()
+        {
+            int[][] expectedArray = {new[] {2, 4, 6}, new[] {3, 5, 7}, new[] {4, 6, 8}};
+            int[][] sortedArray = {new[] {4, 6, 8}, new[] {3, 5, 7}, new[] {2, 4, 6}};
+            
+            IComparator comparator = new AscendingComparator();
+            ISortedSign sumSortedSign = new SumSortedSign();
+
+            BubbleSort.Sort(sortedArray, comparator, sumSortedSign);
+
+            Assert.AreEqual(expectedArray, sortedArray);
+        }
+
+        [TestCase]
+        public void TestSortOfJaggedArrayByDiscendingOfMaxElements()
+        {
+            int[][] expectedArray = { new[] { 4, 6, 8 }, new[] { 3, 5, 7 }, new[] { 2, 4, 6 } };
+            int[][] sortedArray = { new[] { 2, 4, 6 }, new[] { 3, 5, 7 }, new[] { 4, 6, 8 } };
+
+            IComparator comparator = new DiscendingComparator();
+            ISortedSign maxElementSortedSign = new MaxElementSortedSign();
+
+            BubbleSort.Sort(sortedArray, comparator, maxElementSortedSign);
+
+            Assert.AreEqual(expectedArray, sortedArray);
+        }
+
+        [TestCase]
+        public void TestSortOfJaggedArrayWithNullElementsByAscendingOfMaxElements()
+        {
+            int[][] expectedArray = { null, new[] { 2, 4, 6 }, new[] { 4, 6, 8 } };
+            int[][] sortedArray = { new[] { 2, 4, 6 }, null, new[] { 4, 6, 8 } };
+
+            IComparator comparator = new AscendingComparator();
+            ISortedSign maxElementSortedSign = new MaxElementSortedSign();
+
+            BubbleSort.Sort(sortedArray, comparator, maxElementSortedSign);
+
+            Assert.AreEqual(expectedArray, sortedArray);
+        }
+
+        [TestCase]
+        public void TestSortOfJaggedArrayWithEmptyArraysByAscendingMaxElements()
+        {
+            int[][] expectedArray = { new int[0], new[] { 2, 4, 6 }, new[] { 4, 6, 8 } };
+            int[][] sortedArray = { new[] { 2, 4, 6 }, new int[0], new[] { 4, 6, 8 } };
+
+            IComparator comparator = new AscendingComparator();
+            ISortedSign maxElementSortedSign = new MaxElementSortedSign();
+
+            BubbleSort.Sort(sortedArray, comparator, maxElementSortedSign);
+
+            Assert.AreEqual(expectedArray, sortedArray);
+        }
+
+        [TestCase]
+        public void TestSortOfJaggedArrayWithEmptyAndNullsArraysByAscendingMaxElements()
+        {
+            int[][] expectedArray = { null, null, new int[0], new int[0], new[] { 2, 4, 6 }, new[] { 4, 6, 8 } };
+            int[][] sortedArray = { new[] { 2, 4, 6 }, null, new int[0], new int[0], new[] { 4, 6, 8 }, null };
+
+            IComparator comparator = new AscendingComparator();
+            ISortedSign maxElementSortedSign = new MaxElementSortedSign();
+
+            BubbleSort.Sort(sortedArray, comparator, maxElementSortedSign);
+
+            Assert.AreEqual(expectedArray, sortedArray);
+        }
+    }
+}
--- Sorter/BubbleSort.cs
+++ Sorter/BubbleSort.cs
@@ -1,161 +1,67 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Sorter
 {
+    public interface IComparator
+    {
+        bool Compare(int a, int b);
+    }
+
+    public interface ISortedSign
+    {
+        int GetSortedSign(int[] array);
+    }
+
     static public class BubbleSort
     {
-        private struct ArrayStruct
+        static public void Sort(int[][] sourceArray, IComparator comparator, ISortedSign sortedSign)
         {
-            public int[] array;
-            public int comparedElement;
-        }
-
-        private delegate bool Comparer(int element1, int element2);
-        private delegate int GetComparedElement(int[] array);
-        static private Comparer compareAscending = (x, y) => x > y;
-        static private Comparer compareDiscending = (x, y) => x < y;
-        static private GetComparedElement getMinElement = (int[] array) => GetElement(array, compareAscending);
-        static private GetComparedElement getMaxElement = (int[] array) => GetElement(array, compareDiscending); 
-
-        static public void SortByMaxSum(int[][] sourceArray, bool isSortAscending = true)
-        {
-            Sort(sourceArray, isSortAscending, GetSumOfArrayElements);
-        }
-
-        static public void SortByMaxElement(int[][] sourceArray, bool isSortAscending = true)
-        {
-            Sort(sourceArray, isSortAscending, getMaxElement);
-        }
-
-        static public void SortByMinElement(int[][] sourceArray, bool isSortAscending = true)
-        {
-            Sort(sourceArray, isSortAscending, getMinElement);
-        }
-
-        static private void Sort(int[][] sourceArray, bool isSortAscending, GetComparedElement getComparedElementFunc)
-        {
-            if (sourceArray != null)
+            if (sourceArray == null)
             {
-                if (isSortAscending)
-                {
-                    SortByComparer(sourceArray, getComparedElementFunc, compareAscending);
-                }
-                else
-                {
-                    SortByComparer(sourceArray, getComparedElementFunc, compareDiscending);
-                }
+                throw new ArgumentNullException(nameof(sourceArray));
             }
-            else
+
+            if (comparator == null)
             {
-                throw new Exception("Source array is null");
+                throw new ArgumentNullException(nameof(comparator));
             }
-        }
 
-        static private void SortByComparer(int[][] sourceArray, GetComparedElement getComparedElementFunc, Comparer comparator)
-        {
-            int firstNotNullArrayIndex = RaiseNullArrays(sourceArray);
-            firstNotNullArrayIndex = RaiseZeroArrays(sourceArray, firstNotNullArrayIndex);
-            ArrayStruct[] arrayStructes = GetArrayStructes(sourceArray, firstNotNullArrayIndex, getComparedElementFunc);
-
-            for (int i = firstNotNullArrayIndex; i < arrayStructes.Length - 1; i++)
+            if (sortedSign == null)
             {
-                for (int j = i + 1; j < arrayStructes.Length; j++)
+                throw new ArgumentNullException(nameof(sortedSign));
+            }
+
+            for (int i = 0; i < sourceArray.Length - 1; i++)
+            {
+                for (int j = 0; j < sourceArray.Length - 1; j++)
                 {
-                    if (comparator(arrayStructes[i].comparedElement, arrayStructes[j].comparedElement))
+                    if (sourceArray[j] != null)
                     {
-                        Swap(arrayStructes, i, j);
+                        if ((sourceArray[j + 1] != null) && (sourceArray[j + 1].Length > 0))
+                        {
+                            if (sourceArray[j].Length > 0)
+                            {
+                                if (comparator.Compare(sortedSign.GetSortedSign(sourceArray[j]),
+                                    sortedSign.GetSortedSign(sourceArray[j + 1])))
+                                {
+                                    Swap(ref sourceArray[j], ref sourceArray[j + 1]);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Swap(ref sourceArray[j], ref sourceArray[j + 1]);
+                        }
                     }
                 }
             }
-            for (int i = firstNotNullArrayIndex; i < sourceArray.Length; i++)
-            {
-                sourceArray[i] = arrayStructes[i].array;
-            }
         }
 
-        static private int RaiseNullArrays(int[][] sourceArray)
+        static private void Swap(ref int[] array1, ref int[] array2)
         {
-            int firstNotNullArrayIndex = 0;
-
-            for (int i = firstNotNullArrayIndex; i < sourceArray.Length; i++)
-            {
-                if (sourceArray[i] == null)
-                {
-                    SwapArrays(sourceArray, firstNotNullArrayIndex, i);
-                    firstNotNullArrayIndex++;
-                }
-            }
-            return firstNotNullArrayIndex;
-        }
-
-        static private int RaiseZeroArrays(int[][] sourceArray, int index)
-        {
-            for (int i = index; i < sourceArray.Length; i++)
-            {
-                if (sourceArray[i].Length == 0)
-                {
-                    SwapArrays(sourceArray, index, i);
-                    index++;
-                }
-            }
-            return index;
-        }
-
-        static private ArrayStruct[] GetArrayStructes(int[][] sourceArray, int firstNotNullArrayIndex, GetComparedElement getElemFunc)
-        {
-            ArrayStruct[] arrayStructes = new ArrayStruct[sourceArray.Length];
-
-            for (int i = firstNotNullArrayIndex; i < arrayStructes.Length; i++)
-            {
-                ArrayStruct element = new ArrayStruct();
-                element.array = sourceArray[i];
-                element.comparedElement = getElemFunc(sourceArray[i]);
-                arrayStructes[i] = element;
-            }
-            return arrayStructes;
-        }
-
-        static private int GetSumOfArrayElements(int[] array)
-        {
-            int resultSum = 0;
-
-            for (int i = 0; i < array.Length; i++)
-            {
-                resultSum += array[i];
-            }
-            return resultSum;
-        }
-
-        static private int GetElement(int[] array, Comparer comparator)
-        {
-            int elem = array[0];
-
-            for (int i = 1; i < array.Length; i++)
-            {
-                if (comparator(elem, array[i]))
-                {
-                    elem = array[i];
-                }
-            }
-            return elem;
-        }
-
-        static private void Swap(ArrayStruct[] array, int i, int j)
-        {
-            ArrayStruct temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-        }
-
-        static private void SwapArrays(int[][] array, int i, int j)
-        {
-            int[] temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
+            int[] temp = array2;
+            array2 = array1;
+            array1 = temp;
         }
     }
 }
